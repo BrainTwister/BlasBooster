3d5e75814a654003e893f57af2b0bea0b8c36598
Bernd Doser <bernd.doser@braintwister.eu>
2015-06-16 21:55:00 +0200

    remove CoreLib

--- include/BlasBooster/Core/GenerateTypeMatrix.h
+++ include/BlasBooster/Core/GenerateTypeMatrix.h
@@ -19,18 +19,58 @@ typedef std::pair<size_t,size_t> Dimension;
 typedef Matrix<Dense,Dimension> DimensionMatrix;
 typedef Matrix<Dense,double> NormMatrix;
 
-TypeMatrix generateTypeMatrix(BlockedDenseMatrix const& matrix);
+inline TypeMatrix generateTypeMatrix(BlockedDenseMatrix const& matrix)
+{
+	TypeMatrix typeMatrix(matrix.getNbRows(),matrix.getNbColumns());
 
-DimensionMatrix generateDimensionMatrix(BlockedDenseMatrix const& matrix);
+	BlockedDenseMatrix::const_iterator blockMatrixCur(matrix.begin()), blockMatrixEnd(matrix.end());
+	TypeMatrix::iterator typeMatrixCur(typeMatrix.begin()), typeMatrixEnd(typeMatrix.end());
+	for ( ; blockMatrixCur != blockMatrixEnd; ++blockMatrixCur, ++typeMatrixCur )
+	{
+		*typeMatrixCur = (*blockMatrixCur)->getTypeIndex();
+	}
+
+	return typeMatrix;
+}
+
+inline DimensionMatrix generateDimensionMatrix(BlockedDenseMatrix const& matrix)
+{
+	DimensionMatrix dimMatrix(matrix.getNbRows(),matrix.getNbColumns());
+
+	BlockedDenseMatrix::const_iterator blockMatrixCur(matrix.begin()), blockMatrixEnd(matrix.end());
+	DimensionMatrix::iterator dimMatrixCur(dimMatrix.begin()), dimMatrixEnd(dimMatrix.end());
+	for ( ; blockMatrixCur != blockMatrixEnd; ++blockMatrixCur, ++dimMatrixCur )
+	{
+		dimMatrixCur->first = getNbRows(*blockMatrixCur);
+		dimMatrixCur->second = getNbColumns(*blockMatrixCur);
+	}
+
+	return dimMatrix;
+}
 
 template <> struct TypeName<Dimension>
 {
 	static const std::string value() { return "Dimension"; }
 };
 
-NormMatrix generateNormMatrix(BlockedDenseMatrix const& matrix);
+inline NormMatrix generateNormMatrix(BlockedDenseMatrix const& matrix)
+{
+	NormMatrix normMatrix(matrix.getNbRows(),matrix.getNbColumns());
 
-std::ostream& operator << (std::ostream& stream, Dimension const& dimension);
+	BlockedDenseMatrix::const_iterator blockMatrixCur(matrix.begin()), blockMatrixEnd(matrix.end());
+	NormMatrix::iterator normMatrixCur(normMatrix.begin()), normMatrixEnd(normMatrix.end());
+	for ( ; blockMatrixCur != blockMatrixEnd; ++blockMatrixCur, ++normMatrixCur )
+	{
+		*normMatrixCur = getNorm(*blockMatrixCur);
+	}
+
+	return normMatrix;
+}
+
+inline std::ostream& operator << (std::ostream& stream, Dimension const& dimension)
+{
+    return stream << dimension.first << "x" << dimension.second;
+}
 
 } // namespace BlasBooster
 
