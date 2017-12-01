e05f5569a92bf4adc240d2762e231a213c7246f7
Bernd Doser <bernd.doser@braintwister.eu>
2017-12-01 21:13:14 +0000

    public interface

---
+++ include/BlasBooster/BlasBooster.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2012-2015, Bernd Doser (service@braintwister.eu)
+// All rights reserved.
+//
+// This file is part of BlasBooster
+//
+// ANY USE OF THIS CODE CONSTITUTES ACCEPTANCE OF THE
+// TERMS OF THE COPYRIGHT NOTICE
+
+#ifndef BLASBOOSTER_BLASBOOSTER_H_
+#define BLASBOOSTER_BLASBOOSTER_H_
+
+
+
+#endif // BLASBOOSTER_BLASBOOSTER_H_
