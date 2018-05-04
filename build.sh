128819bea3bdace21d4efe8109504246b4a9d43f
Bernd Doser <bernd.doser@braintwister.eu>
2018-05-04 20:26:18 +0000

    add conan remotes braintwister and bincrafters

--- build.sh
+++ build.sh
@@ -9,14 +9,13 @@ CONAN_DIR="conan$SUFFIX"
 BUILD_DIR="build$SUFFIX"
 export CONAN_USER_HOME=$PWD/$CONAN_DIR
 
-if [ ! -d "$CONAN_DIR" ]; then
-  mkdir $CONAN_DIR
-  conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
-  conan remote add bernddoser https://api.bintray.com/conan/bernddoser/conan --insert
-fi
+mkdir -p $CONAN_DIR
+conan remote add braintwister https://api.bintray.com/conan/braintwister/conan
+conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 rm -fr $BUILD_DIR
-mkdir $BUILD_DIR
+mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 conan install .. --build=missing -s compiler.libcxx=libstdc++11
