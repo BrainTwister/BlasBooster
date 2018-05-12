2fd77159e44759fd8b7db6a75423054705570585
Bernd Doser <bernd.doser@braintwister.eu>
2018-05-12 20:22:54 +0000

    clean up make build type

--- cmake/FindOpenBLAS.cmake
+++ cmake/FindOpenBLAS.cmake
@@ -2,8 +2,6 @@ if (OPENBLAS_FOUND)
   return()
 endif (OPENBLAS_FOUND)
 
-message("OpenBLAS = ${OPENBLAS_ROOT}")
-
 find_library(OPENBLAS_LIBRARY
   NAMES libopenblas.a
   PATHS ${OPENBLAS_ROOT}/lib
