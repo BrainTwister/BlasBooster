feebc5acebaf4ae5463b9a965cbfbd1abb71294b
Bernd Doser <bernd.doser@braintwister.eu>
2018-05-23 13:01:18 +0000

    add build type to jenkins build script

--- build.sh
+++ build.sh
@@ -9,14 +9,16 @@ CONAN_DIR="conan$SUFFIX"
 BUILD_DIR="build$SUFFIX"
 export CONAN_USER_HOME=$PWD/$CONAN_DIR
 
-mkdir -p $CONAN_DIR
-conan remote add braintwister https://api.bintray.com/conan/braintwister/conan
-conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+if [ ! -d "$CONAN_DIR" ]; then
+  mkdir -p $CONAN_DIR
+  conan remote add braintwister https://api.bintray.com/conan/braintwister/conan
+  conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
+  conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+fi
 
 rm -fr $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make 2>&1 |tee make.out
