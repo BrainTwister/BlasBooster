312e6649e103348557c83361eab157fa458f2bb5
Bernd Doser <bernd.doser@braintwister.eu>
2018-05-23 20:25:42 +0000

    some tests with the benchmark

--- src/Benchmark/benchmark.cpp
+++ src/Benchmark/benchmark.cpp
@@ -2,6 +2,7 @@
 #include "BlasBooster/Core/BlockedMatrixGenerator.h"
 #include "BlasBooster/Core/BlockSizeGenerator.h"
 #include "BlasBooster/Core/DenseMatrix.h"
+#include "BlasBooster/Core/GenerateTypeMatrix.h"
 #include "BlasBooster/Core/Multiplication.h"
 #include "BlasBooster/Core/Multiplication_IntelMKL.h"
 #include "BlasBooster/Core/Multiplication_TheBestPolicy.h"
@@ -21,26 +22,27 @@ int main(int argc, char* argv[])
 
         std::cout << "BlasBooster " + version + " Benchmark" << std::endl;
 
-        Threshold threshold(ThresholdSettings(JSON{R"(
-            {
-                "significanceThresholdSinglePrecision":                                    1e-5,  
-                "significanceThresholdDoublePrecision":                                    1e-10, 
-                "occupationThresholdMatrixSparseFloatSinglePrecision":                     0.0,   
-                "occupationThresholdMatrixSparseFloatDoublePrecision":                     0.1,   
-                "occupationThresholdMatrixDenseFloatSinglePrecision":                      0.0,   
-                "occupationThresholdMatrixDenseFloatDoublePrecision":                      1.0,   
-                "occupationThresholdMatrixSparseDoubleSinglePrecision":                    0.1,   
-                "occupationThresholdMatrixSparseDoubleDoublePrecision":                    0.1,   
-                "occupationThresholdMultipleMatrixSparseDoubleSparseFloatSinglePrecision": 0.1,   
-                "occupationThresholdMultipleMatrixSparseDoubleSparseFloatDoublePrecision": 0.2,   
-                "occupationThresholdMultipleMatrixDenseFloatSparseDoubleSinglePrecision":  0.1,   
-                "occupationThresholdMultipleMatrixDenseFloatSparseDoubleDoublePrecision":  1.0,   
-                "occupationThresholdMatrixDenseDoubleSinglePrecision":                     1.0,   
-                "occupationThresholdMatrixDenseDoubleDoublePrecision":                     1.0    
-            }
-        )"}));
+        Threshold threshold;
+//        Threshold threshold(ThresholdSettings(JSON{R"(
+//            {
+//                "significanceThresholdSinglePrecision":                                    1e-5,
+//                "significanceThresholdDoublePrecision":                                    1e-10,
+//                "occupationThresholdMatrixSparseFloatSinglePrecision":                     0.0,
+//                "occupationThresholdMatrixSparseFloatDoublePrecision":                     0.1,
+//                "occupationThresholdMatrixDenseFloatSinglePrecision":                      0.0,
+//                "occupationThresholdMatrixDenseFloatDoublePrecision":                      1.0,
+//                "occupationThresholdMatrixSparseDoubleSinglePrecision":                    0.1,
+//                "occupationThresholdMatrixSparseDoubleDoublePrecision":                    0.1,
+//                "occupationThresholdMultipleMatrixSparseDoubleSparseFloatSinglePrecision": 0.1,
+//                "occupationThresholdMultipleMatrixSparseDoubleSparseFloatDoublePrecision": 0.2,
+//                "occupationThresholdMultipleMatrixDenseFloatSparseDoubleSinglePrecision":  0.1,
+//                "occupationThresholdMultipleMatrixDenseFloatSparseDoubleDoublePrecision":  1.0,
+//                "occupationThresholdMatrixDenseDoubleSinglePrecision":                     1.0,
+//                "occupationThresholdMatrixDenseDoubleDoublePrecision":                     1.0
+//            }
+//        )"}));
 
-        DynamicMatrix F(new Matrix<Dense,double>(1000, 1000, AllFiller<double>(1.e-6)));
+        DynamicMatrix F(new Matrix<Dense,double>(1000, 1000, AllFiller<double>(1e-8)));
         DynamicMatrix Z(new Matrix<Dense,double>(1000, 1000, AllFiller<double>(0.0)));
 
         BlockedDenseMatrix block{{Z,Z,Z,Z},{Z,F,Z,Z},{Z,Z,Z,Z},{Z,Z,Z,Z}};
@@ -62,6 +64,12 @@ int main(int argc, char* argv[])
             std::cout << "  2-norm = " << norm<NormTwo>(C - refC) << std::endl;
         }
         {
+            ScopedTimer scopedTimer("OpenBLAS sgemm 1000x1000");
+            Matrix<Dense, float> A(1000, 1000, AllFiller<double>(1.0));
+            Matrix<Dense, float> B(1000, 1000, AllFiller<double>(1.0));
+            auto C = (A * B).template execute<OpenBLAS>();
+        }
+        {
             ScopedTimer scopedTimer("sparse double");
             Matrix<Sparse, double> A(refA, AbsoluteValueRangeChecker<ThresholdType>(threshold.getSignificanceThreshold<double>()));
             Matrix<Sparse, double> B(refB, AbsoluteValueRangeChecker<ThresholdType>(threshold.getSignificanceThreshold<double>()));
@@ -75,12 +83,16 @@ int main(int argc, char* argv[])
             ScopedTimer scopedTimer("blocked multiplication");
 
             std::unique_ptr<ScopedTimer> ptrScopedTimer(new ScopedTimer("blocked multiplication, block size"));
-            auto blockSizeA = BlockSizeGenerator(50, 200)(refA);
-            auto blockSizeB = BlockSizeGenerator(50, 200)(refB);
+            auto blockSizeA = BlockSizeGenerator(500, 1000)(refA);
+            auto blockSizeB = BlockSizeGenerator(500, 1000)(refB);
 
             ptrScopedTimer.reset(new ScopedTimer("blocked multiplication, blocking"));
             BlockedDenseMatrix A = BlockedMatrixGenerator()(refA, blockSizeA.first, blockSizeA.second, threshold);
             BlockedDenseMatrix B = BlockedMatrixGenerator()(refB, blockSizeB.first, blockSizeB.second, threshold);
+
+            std::cout << generateTypeMatrix(A) << std::endl;
+            std::cout << generateDimensionMatrix(A) << std::endl;
+            //std::cout << generateNormMatrix(A) << std::endl;
 
             ptrScopedTimer.reset(new ScopedTimer("blocked multiplication, mult"));
             BlockedDenseMatrix C = (A * B).template execute<TheBestPolicy>();
