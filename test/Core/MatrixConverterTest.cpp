f8d048b6235947bbf3d245f823bf6a131f5e1a61
Bernd Doser <bernd.doser@braintwister.eu>
2018-06-06 13:40:04 +0000

    Fix ThresholdSettings in MatrixConverterTest

--- test/Core/MatrixConverterTest.cpp
+++ test/Core/MatrixConverterTest.cpp
@@ -18,22 +18,26 @@ protected:
 
     virtual void SetUp()
     {
-    	threshold = Threshold(ThresholdSettings(
-			1e-5, // significanceThresholdFloatPrecision
-			1e-10, // significanceThresholdDoublePrecision
-			0.0,  // occupationThresholdMatrixSparseFloatSinglePrecision
-			0.5,  // occupationThresholdMatrixSparseFloatDoublePrecision
-			0.0,  // occupationThresholdMatrixDenseFloatSinglePrecision
-			1.0,  // occupationThresholdMatrixDenseFloatDoublePrecision
-			0.5,  // occupationThresholdMatrixSparseDoubleSinglePrecision
-			0.5,  // occupationThresholdMatrixSparseDoubleDoublePrecision
-			0.5,  // occupationThresholdMultipleMatrixSparseDoubleSparseFloatSinglePrecision
-			1.0,  // occupationThresholdMultipleMatrixSparseDoubleSparseFloatDoublePrecision
-			0.5,  // occupationThresholdMultipleMatrixDenseFloatSparseDoubleSinglePrecision
-			1.0,  // occupationThresholdMultipleMatrixDenseFloatSparseDoubleDoublePrecision
-			1.0,  // occupationThresholdMatrixDenseDoubleSinglePrecision
-			1.0   // occupationThresholdMatrixDenseDoubleDoublePrecision
-		));
+		threshold = Threshold(ThresholdSettings()
+    	    .set_significanceThresholdSinglePrecision(1e-5)
+		    .set_significanceThresholdDoublePrecision(1e-10)
+		    .set_occupationThresholdMatrixZeroSinglePrecision(0.0)
+		    .set_occupationThresholdMatrixZeroDoublePrecision(0.0)
+		    .set_occupationThresholdMatrixSparseFloatSinglePrecision(0.0)
+		    .set_occupationThresholdMatrixSparseFloatDoublePrecision(0.5)
+		    .set_occupationThresholdMatrixDenseFloatSinglePrecision(0.0)
+		    .set_occupationThresholdMatrixDenseFloatDoublePrecision(1.0)
+		    .set_occupationThresholdMatrixSparseDoubleSinglePrecision(0.5)
+		    .set_occupationThresholdMatrixSparseDoubleDoublePrecision(0.5)
+		    .set_occupationThresholdMultipleMatrixSparseDoubleSparseFloatSinglePrecision(0.5)
+		    .set_occupationThresholdMultipleMatrixSparseDoubleSparseFloatDoublePrecision(1.0)
+		    .set_occupationThresholdMultipleMatrixDenseFloatSparseDoubleSinglePrecision(0.5)
+		    .set_occupationThresholdMultipleMatrixDenseFloatSparseDoubleDoublePrecision(1.0)
+		    .set_occupationThresholdMatrixDenseDoubleSinglePrecision(1.0)
+		    .set_occupationThresholdMatrixDenseDoubleDoublePrecision(1.0)
+		);
+
+		std::cout << threshold.getSignificanceThreshold<double>() << std::endl;
     }
 
 	Threshold threshold;
@@ -46,13 +50,15 @@ TEST_F(MatrixConverterTest, ConvertDenseToSparse)
 		{ 1e-6 , 1e-12 }
 	};
 
-	EXPECT_DOUBLE_EQ(0.5,A.getOccupation(AbsoluteValueRangeChecker<ThresholdType>(threshold.getSignificanceThreshold<double>())));
-	EXPECT_DOUBLE_EQ(0.0,A.getOccupation(AbsoluteValueRangeChecker<ThresholdType>(threshold.getSignificanceThreshold<float>())));
+	EXPECT_DOUBLE_EQ(1e-10, threshold.getSignificanceThreshold<double>());
+
+	EXPECT_DOUBLE_EQ(0.5, A.getOccupation(AbsoluteValueRangeChecker<ThresholdType>(threshold.getSignificanceThreshold<double>())));
+	EXPECT_DOUBLE_EQ(0.0, A.getOccupation(AbsoluteValueRangeChecker<ThresholdType>(threshold.getSignificanceThreshold<float>())));
 
 	auto B = MatrixConverter()(A,threshold);
 
 	size_t typeIndex = Matrix<Sparse,float>::typeIndex_;
-	EXPECT_EQ(typeIndex,B->getTypeIndex());
+	EXPECT_EQ(typeIndex, B->getTypeIndex());
 
 	Matrix<Dense,double> C {
 		{ 0.0 , 1e-6 },
