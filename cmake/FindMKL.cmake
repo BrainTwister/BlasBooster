b4762d8974d058ff5d4f9c870d2192296e1386bc
Bernd Doser <bernd.doser@braintwister.eu>
2018-07-25 13:41:55 +0000

    Find libiomp5 for IntelMKL

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -7,12 +7,14 @@ find_path(MKL_INCLUDE_DIR mkl.h $ENV{MKLROOT}/include)
 find_library(MKL_INTEL_LP64_LIBRARY mkl_intel_lp64 $ENV{MKLROOT}/lib/intel64)
 find_library(MKL_CORE_LIBRARY mkl_core $ENV{MKLROOT}/lib/intel64)
 find_library(MKL_GNU_THREAD_LIBRARY mkl_gnu_thread $ENV{MKLROOT}/lib/intel64)
+find_library(MKL_IOMP5_LIBRARY iomp5 $ENV{MKLROOT}/../compiler/lib/intel64)
 
 list(APPEND MKL_LIBRARIES
   ${MKL_INTEL_LP64_LIBRARY}
   ${MKL_CORE_LIBRARY}
   ${MKL_GNU_THREAD_LIBRARY}
-  iomp5 dl pthread m
+  ${MKL_IOMP5_LIBRARY}
+  dl pthread m
 )
 
 include(FindPackageHandleStandardArgs)
@@ -20,6 +22,7 @@ find_package_handle_standard_args(MKL DEFAULT_MSG
   MKL_INTEL_LP64_LIBRARY
   MKL_CORE_LIBRARY
   MKL_GNU_THREAD_LIBRARY
+  MKL_IOMP5_LIBRARY
 )
 
 mark_as_advanced(MKL_LIBRARIES)
