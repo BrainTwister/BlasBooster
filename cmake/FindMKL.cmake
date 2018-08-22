5dc9892063070c948ff3ffc493c231af0e80e546
Bernd Doser <bernd.doser@braintwister.eu>
2018-08-22 13:00:25 +0000

    add path for IntelMKL 2018 update 3

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -7,7 +7,7 @@ find_path(MKL_INCLUDE_DIR mkl.h $ENV{MKLROOT}/include)
 find_library(MKL_INTEL_LP64_LIBRARY mkl_intel_lp64 $ENV{MKLROOT}/lib/intel64)
 find_library(MKL_CORE_LIBRARY mkl_core $ENV{MKLROOT}/lib/intel64)
 find_library(MKL_GNU_THREAD_LIBRARY mkl_gnu_thread $ENV{MKLROOT}/lib/intel64)
-find_library(MKL_IOMP5_LIBRARY iomp5 $ENV{MKLROOT}/../compiler/lib/intel64)
+find_library(MKL_IOMP5_LIBRARY iomp5 $ENV{MKLROOT}/lib/intel64 $ENV{MKLROOT}/../compiler/lib/intel64)
 
 list(APPEND MKL_LIBRARIES
   ${MKL_INTEL_LP64_LIBRARY}
