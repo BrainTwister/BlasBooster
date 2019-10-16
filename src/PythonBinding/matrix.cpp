3036045d06148e02f0faaa8a6962cd6a285ddf5b
Bernd Doser <bernd.doser@braintwister.eu>
2019-10-16 22:54:25 +0200

    update pybind11

--- src/PythonBinding/matrix.cpp
+++ src/PythonBinding/matrix.cpp
@@ -1,13 +1,27 @@
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
-#include "BlasBooster/Core/DenseMatrix.h"
 
-namespace py = pybind11;
-using namespace BlasBooster;
+#include "BlasBooster/Core/DenseMatrix.h"
+#include "BlasBooster/Core/DynamicMatrix.h"
+#include "BlasBooster/Utilities/Version.h"
 
 PYBIND11_MODULE(blasbooster, m)
 {
-    m.doc() = "pybind11 blasbooster plugin";
+    namespace py = pybind11;
+    namespace bb = BlasBooster;
 
-    py::class_<Matrix<Dense, double>>(m, "Matrix")
-        .def(py::init<int, int>());
+    m.doc() = "BlasBooster";
+    m.attr("__version__") = std::string(PROJECT_VERSION) + " revision " + std::string(GIT_REVISION);
+
+    py::class_<bb::DynamicMatrix>(m, "Matrix", py::buffer_protocol())
+		.def(py::init([](py::buffer b)
+		{
+			py::buffer_info info = b.request();
+			return new bb::DynamicMatrix;
+		}));
+//		.def_buffer([](bb::DynamicMatrix &m) -> py::buffer_info
+//		{
+//			return py::buffer_info();
+//		});
 }
